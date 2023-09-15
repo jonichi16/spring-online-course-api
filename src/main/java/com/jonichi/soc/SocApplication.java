3884cdfff87685b6d83af84025fd364ae4af2e7c
jhonel.villanueva <jhonel.villanueva@pointwest.com.ph>
2023-09-15 08:17:03 +0800

    Initial commit

---
+++ src/main/java/com/jonichi/soc/SocApplication.java
@@ -0,0 +1,15 @@
+package com.jonichi.soc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonichi/soc/SocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonichi.soc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
